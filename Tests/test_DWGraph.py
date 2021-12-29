b4671a8a20eb6186b18fb314de55d19e081274b5
SariSafe13 <ssriseef@gmail.com>
2021-12-29 23:07:55 +0200

    need to check just tow functions

--- Tests/test_DWGraph.py
+++ Tests/test_DWGraph.py
@@ -62,3 +62,152 @@ class TestDWGraph(TestCase):
         self.assertEqual(x, y)
 
 
+    def test_get_all_v(self):
+        gr = DWGraph()
+        for n in range(7):
+            gr.add_node(n)
+
+        gr.add_edge(4, 1, 1)
+        gr.add_edge(1, 0, 0.5)
+        gr.add_edge(1, 2, 1.313)
+        gr.add_edge(7, 2, 12)
+        gr.add_edge(1, 7, 40)
+        gr.add_edge(4, 5, 12)
+        gr.add_edge(3, 4, 22)
+        gr.add_edge(4, 3, 13)
+        gr.add_edge(6, 5, 19)
+
+        nodes = gr.get_all_v()
+
+        assert len(nodes) == 6+1
+
+        assert isinstance(nodes, dict)
+        for i in range(6):
+            assert i in nodes.keys()
+
+    def test_all_in_edges_of_node(self):
+        gr = DWGraph()
+        for n in range(7):
+            gr.add_node(n)
+
+        gr.add_edge(0, 1, 1)
+        gr.add_edge(1, 0, 112)
+        gr.add_edge(1, 2, 1.3)
+        gr.add_edge(2, 3, 1.1)
+        gr.add_edge(1, 3, 10)
+        gr.add_edge(2, 1, 10)
+        gr.add_edge(4, 5, 12)
+        gr.add_edge(3, 4, 22)
+        gr.add_edge(4, 6, 3)
+        gr.add_edge(6, 3, 18)
+
+        edge_in = gr.all_in_edges_of_node(0)
+        assert 1 in edge_in
+
+        edge_in = gr.all_in_edges_of_node(1)
+        assert 0 not in edge_in
+
+        edge_in = gr.all_in_edges_of_node(3)
+        for a in [1,2,6]:
+            assert a in edge_in.values()
+
+
+
+    def test_all_out_edges_of_node(self):
+        gr = DWGraph()
+
+        for n in range(5):
+            gr.add_node(n)
+
+        gr.add_edge(0, 1, 1)
+        gr.add_edge(1, 0, 17)
+        gr.add_edge(2, 1, 10)
+        gr.add_edge(2, 4, 1.15)
+
+        edge_in = gr.all_out_edges_of_node(2)
+        assert 1 in edge_in
+
+        edge_in = gr.all_out_edges_of_node(4)
+        assert 5 in edge_in
+
+        assert isinstance(edge_in, dict) == True
+
+    def test_get_mc(self):
+        gr = DWGraph()
+
+        gr.add_node(1)
+        gr.add_node(2)
+        gr.add_node(3)
+        gr.add_node(4)
+        gr.add_node(5)
+        gr.add_node(6)
+        gr.add_node(7)
+
+        gr.remove_node(7)
+        gr.remove_node(1)
+
+        gr.add_node(1)
+        gr.add_edge(1, 2, 1.3)
+        gr.add_edge(2, 3, 1.1)
+        gr.add_edge(1, 3, 10)
+        gr.add_edge(4, 5, 12)
+        gr.add_edge(3, 4, 22)
+        gr.add_edge(4, 6, 3)
+        gr.add_edge(6, 3, 18)
+        gr.remove_edge(1,2)
+
+        mc = gr.get_mc()
+        gr.add_node(7)
+        gr.add_node(8)
+        assert gr.get_mc() == mc + 2
+        gr.remove_node(8)
+        assert gr.get_mc() == mc + 3
+
+    def test_add_edge(self):
+        gr = DWGraph()
+        for n in range(7):
+            gr.add_node(n)
+        gr.add_edge(0, 1, 1)
+        assert gr.e_size() == 1
+
+        gr.add_edge(1, 0, 1.1)
+        assert gr.e_size() == 1+1
+
+        gr.add_edge(1, 3, 10)
+        gr.add_edge(4, 5, 12)
+        gr.add_edge(3, 4, 22)
+        gr.add_edge(4, 6, 3)
+        gr.add_edge(6, 3, 18)
+        assert not 14 in gr.all_in_edges_of_node(4)
+
+    def test_add_node(self):
+        gr = DWGraph()
+        for n in range(8):
+            gr.add_node(n)
+        assert gr.v_size()==8
+        gr.add_node(2)
+        gr.add_node(1)
+        gr.add_node(12)
+        assert gr.v_size() == 8+1
+
+    def test_remove_edge(self):
+        gr = DWGraph()
+        for n in range(7):
+            gr.add_node(n)
+
+        gr.add_edge(4, 1, 1)
+        gr.add_edge(1, 4, 1.1)
+        gr.add_edge(1, 2, 1.3)
+        gr.add_edge(2, 3, 98.31)
+        gr.add_edge(1, 3, 1.55)
+
+        assert gr.e_size() == 5
+        gr.remove_edge(1,3)
+        gr.add_edge(2, 1, 1.55)
+        gr.remove_edge(1, 2)
+        assert gr.e_size() == 4
+        gr.remove_edge(1, 2)
+        assert gr.e_size() == 4
+        #here we remove something not exist then no changes on e_size
+        gr.remove_edge(1, 2)
+        assert gr.e_size() == 4+0
